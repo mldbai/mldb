b151096c35d074b1343cd0908d611a94427665cf
Guy Dumais <guy@datacratic.com>
2015-12-14 12:37:41 -0500

    [MDB-1190] fixed the wrong cast in ReadThroughBindingContext

--- testing/MLDB-1190_segfault_sqlexpr_jseval.py
+++ testing/MLDB-1190_segfault_sqlexpr_jseval.py
@@ -31,13 +31,35 @@ rez = mldb.perform("PUT", "/v1/functions/getFeatVec", [], conf)
 # This segfaults after a couple of calls
 for i in range(25):
     rez = mldb.perform("GET", "/v1/query", 
-        [["q", """select getFeatVec({raw_text: 'I really loved this party!!!',
-                                     preProcessed: 'I really loved this party!!!'}) as *"""]])
-    rez = mldb.perform("GET", "/v1/query", 
-        [["q", """select getFeatVec({raw_text: 'I really loved this party!!!',
-                                     preProcessed: 'I really loved this party!!!'})"""]])
+                       [["q", """select getFeatVec({raw_text: 'I really loved this party!!!',
+                       preProcessed: 'I really loved this party!!!'}) as *"""]])
 
 jsRes = json.loads(rez["response"])
+mldb.log(jsRes)
+
+assert rez['statusCode'] == 200, 'expected query to return 200'
+expected_response = [
+   {
+      "columns" : [
+         [ "msgStats.msgLen", 28, "-Inf" ],
+         [ "words.party", 1, "-Inf" ],
+         [ "words.this", 1, "-Inf" ],
+         [ "words.really", 1, "-Inf" ],
+         [ "words.loved", 1, "-Inf" ],
+         [ "words.I", 1, "-Inf" ]
+      ]
+   }
+]
+assert jsRes == expected_response, 'expected responses to match'
+
+for i in range(25):
+    rez = mldb.perform("GET", "/v1/query", 
+                       [["q", """select getFeatVec({raw_text: 'I really loved this party!!!',
+                       preProcessed: 'I really loved this party!!!'})"""]])
+
+jsRes = json.loads(rez["response"])
+assert rez['statusCode'] == 200, 'expected query to return 200'
+
 mldb.log(jsRes)
 
 mldb.script.set_return("success")
