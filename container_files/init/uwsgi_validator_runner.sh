2777e159aa4a8ef6540f34270e3908f2ae964f3b
Jean Raby <jean@datacratic.com>
2016-01-14 12:01:03 -0500

    Update services to use 'exec' and drop tee -a log
    Allows all services to be properly controlled by sv {status,start,stop} service
    
    The in-container log file for all services will not be generated anymore.
    If we ever need those, we can use a 'log' script like mldb_runner does.

--- container_files/init/uwsgi_validator_runner.sh
+++ container_files/init/uwsgi_validator_runner.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+exec 2>&1  # stderr to stdout for logging purposes
+
 install -o _mldb -d -m 750 /var/log/validator_api
 
 install -d -o _mldb -g _mldb {{MLDB_VALIDATOR_DIR}}
 export CONFIG=/etc/validator_api_config.json
-/sbin/setuser _mldb /usr/local/bin/uwsgi \
+exec /sbin/setuser _mldb /usr/local/bin/uwsgi \
     --socket 127.0.0.1:{{UWSGI_VALIDATOR_PORT}} \
     --master \
+    --die-on-term \
     --processes 8 \
     --logto=/var/log/validator_api/validator_api.log \
     --chdir=/opt/bin \
