6a127110277d7aa9d42238d2ac2bb826b4f3b8f8
Jeremy Barnes <jeremy@datacratic.com>
2016-01-24 14:44:21 -0500

    Reduce over-production of rows on rowHash ordered queries with simple limit (MLDB-1286)

--- server/bound_queries.cc
+++ server/bound_queries.cc
@@ -1,7 +1,7 @@
-// This file is part of MLDB. Copyright 2015 Datacratic. All rights reserved.
-
 /** bound_queries.cc
     Jeremy Barnes, 12 August 2015
+
+    This file is part of MLDB. Copyright 2015 Datacratic. All rights reserved.
 
     Bound version of SQL queries.
 */
@@ -454,7 +454,13 @@ struct RowHashOrderedExecutor: public BoundSelectQuery::Executor {
 
         auto doRow = [&] (ssize_t rowNum) -> bool
             {
-                QueryThreadTracker childTracker = std::move(parentTracker.child());
+                //if (rowNum % 1000 == 0)
+                //    cerr << "doing row " << rowNum << " with minRowNum "
+                //         << minRowNum << " maxRowNumNeeded " << maxRowNumNeeded
+                //         << " maxRowNum " << maxRowNum << endl;
+
+                QueryThreadTracker childTracker
+                    = std::move(parentTracker.child());
 
                 MatrixNamedRow row;
                 try {
@@ -545,15 +551,35 @@ struct RowHashOrderedExecutor: public BoundSelectQuery::Executor {
                  && (limit == -1 || sorted.size() < offset + limit);  ++i)
             doRow(i);
 
+        //cerr << "Done first " << NUM_TO_SAMPLE << " rows with "
+        //     << sorted.size() << " total" << endl;
 
-        if ((limit == -1 || sorted.size() < offset + limit)
-            && rows.size() > NUM_TO_SAMPLE) {
-            cerr << "hit rate is " << sorted.size() << "%" << endl;
-            // TODO: knowing the hit rate, we can start off with a
-            // smaller number of rows to test
-            ML::run_in_parallel_blocked(NUM_TO_SAMPLE, rows.size(), doRow);
+        size_t numProcessed = std::min(NUM_TO_SAMPLE, rows.size());
+        while (limit == -1 || sorted.size() < offset + limit) {
+            double hitRate = 1.0 * sorted.size() / numProcessed;
+            size_t numRequired = rows.size();
+            if (limit != -1) {
+                // Project how many we need to get to the limit
+                numRequired = (offset + limit) / hitRate;
+            }
+
+            cerr << "hit rate after " << numProcessed << " is "
+                 << 100.0 * hitRate << "%; estimate that we need to scan "
+                 << numRequired << " rows in total" << endl;
+
+            // Do another block
+            if (numRequired - numProcessed < 100) {
+                for (size_t n = numProcessed;  n < numRequired;  ++n) {
+                    doRow(n);
+                }
+            }
+            else {
+                ML::run_in_parallel_blocked(numProcessed, numRequired, doRow);
+            }
+
+            numProcessed = numRequired;
         }
-        
+
         //cerr << "got " << maxRowNumNeeded << " rows with min row num " << minRowNum << endl;
         //cerr << "sorted.size() = " << sorted.size() << endl;
         cerr << "Scanned " << sorted.size() << " in " << scanTimer.elapsed()
