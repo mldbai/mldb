e623fb48162cb87c4871867213f53b1908fe650b
Francois-Michel L Heureux <fmlheureux@datacratic.com>
2016-02-02 16:42:58 -0500

    MLDBFB-335 when timestamp variable test

--- testing/testing.mk
+++ testing/testing.mk
@@ -308,3 +308,4 @@ $(eval $(call mldb_unit_test,MLDB-1315-row-table-expressions.js))
 $(eval $(call mldb_unit_test,MLDB-1345-having.py,,manual)) # manual -- awaiting fix
 $(eval $(call mldb_unit_test,mldb_unit_test_test.py))
 $(eval $(call mldb_unit_test,MLDB-1216-fetcher-function.js))
+$(eval $(call mldb_unit_test,MLDBFB-335_when_timestamp_variable_test.py))
