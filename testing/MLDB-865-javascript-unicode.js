3cad32c9a043a170835674264827693a572df050
Francois-Michel L Heureux <fmlheureux@datacratic.com>
2016-02-10 17:58:39 -0500

    MLDB-1365 update js test

--- testing/MLDB-865-javascript-unicode.js
+++ testing/MLDB-865-javascript-unicode.js
@@ -1,34 +1,101 @@
-// This file is part of MLDB. Copyright 2015 Datacratic. All rights reserved.
+/**
+ * MLDB-865-javascript-unicode.js
+ * Datacratic, 2015
+ * Mich, 2016-02-10
+ * This file is part of MLDB. Copyright 2015 Datacratic. All rights reserved.
+ **/
 
 function succeeded(response)
 {
-        return response.responseCode >= 200 && response.responseCode < 400;
+    return response.responseCode >= 200 && response.responseCode < 400;
 }
 
-function assertSucceeded(process, response)
+function assertSucceeded(response)
 {
-        plugin.log(process, response);
+    plugin.log(response);
 
-        if (!succeeded(response)){
-            throw process + " failed: " + JSON.stringify(response);
-        }
+    if (!succeeded(response)) {
+        throw process + " failed: " + JSON.stringify(response);
+    }
 }
 
 
-var ds = mldb.createDataset({"id": "hellô", "type":"embedding"})
+var ds = mldb.createDataset({"id": "hellô", "type":"embedding"});
 
 ds.commit();
 
-assertSucceeded("", mldb.get("/v1/query", {"q": 'select * from "hellô"'}))
+assertSucceeded(mldb.get("/v1/query", {"q": 'select * from "hellô"'}));
 
-assertSucceeded("", mldb.get("/v1/datasets"))
+assertSucceeded(mldb.get("/v1/datasets"));
 
-assertSucceeded("", mldb.put("/v1/datasets/hôwdy", {"type": 'embedding'}))
+assertSucceeded(mldb.put("/v1/datasets/hôwdy", {"type": 'embedding'}));
 
-assertSucceeded("", mldb.get("/v1/datasets"))
+assertSucceeded(mldb.get("/v1/datasets"));
 
-assertSucceeded("", mldb.post("/v1/datasets", {"type": 'embedding', "id": "hî" }))
+assertSucceeded(mldb.post("/v1/datasets", {"type": 'embedding', "id": "hî" }));
 
-assertSucceeded("", mldb.perform("GET", "/v1/datasets"))
+assertSucceeded(mldb.perform("GET", "/v1/datasets"));
+
+function executeSequence(_id) {
+    var url = '/v1/datasets/' + encodeURIComponent(_id)
+    mldb.log(url)
+
+    mldb.put(url, {
+        'type' : 'sparse.mutable'
+    })
+    res = mldb.get(url)
+    assertSucceeded(res);
+    if (res.json["id"] != _id) {
+        throw res.json["id"] + " != " + _id;
+    }
+
+    mldb.del(url);
+    res = mldb.get(url);
+    if (res.responseCode != 404) {
+        throw res.responseCode + " != 404";
+    }
+
+    mldb.post('/v1/datasets', {
+        'id' : _id,
+        'type' : 'sparse.mutable'
+    })
+    res = mldb.get(url)
+    assertSucceeded(res);
+    if (res.json["id"] != _id) {
+        throw res.json["id"] + " != " + _id;
+    }
+
+    mldb.del(url);
+    res = mldb.get(url);
+    if (res.responseCode != 404) {
+        throw res.responseCode + " != 404";
+    }
+}
+
+executeSequence("françois");
+executeSequence("françois/michel");
+executeSequence("françois michel");
+executeSequence('"françois says hello/goodbye, eh?"');
+
+mldb.put('/v1/datasets/ds', {
+    'type' : 'sparse.mutable'
+});
+mldb.post('/v1/datasets/ds/commit');
+
+mldb.put('/v1/procedures/' + encodeURIComponent('françois'), {
+    'type' : 'transform',
+    'params' : {
+        'inputData' : 'SELECT * FROM ds',
+        'outputDataset' : {
+            'id' : 'outDs',
+            'type' : 'sparse.mutable'
+        }
+    }
+});
+
+url = '/v1/procedures/' + encodeURIComponent('françois') + '/runs/'
+      + encodeURIComponent('michêl');
+mldb.put(url)
+mldb.get(url)
 
 "success"
