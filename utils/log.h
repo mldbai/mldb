32af094bf23cabb5f9bd4a482004949d5bc5ef2e
Guy Dumais <guy@datacratic.com>
2016-03-07 13:17:17 -0500

    [MLDB-1342] added missing file

---
+++ utils/log.h
@@ -0,0 +1,12 @@
+/* log.h                                                           -*- C++ -*-
+   Guy Dumais, 7 March 2016
+
+   This file is part of MLDB. Copyright 2016 Datacratic. All rights reserved.
+
+   Include this file where the logging interface is used.
+*/
+
+#pragma once
+
+#include "log_fwd.h"
+#include "mldb/ext/spdlog/include/spdlog/spdlog.h"
