57eae9d103b31f9da1dcb187011b935bb39d648a
Jeremy Barnes <jeremy@datacratic.com>
2016-03-10 14:30:35 -0500

    Implemented BSON to expression (MLDB-1412)

--- mongodb/mongodb_plugin.cc
+++ mongodb/mongodb_plugin.cc
@@ -143,6 +143,7 @@ struct MongoDataset: public Dataset {
 /*****************************************************************************/
 
 struct MongoImportConfig: public ProcedureConfig {
+    static constexpr const char * name = "mongodb.import";
 };
 
 DECLARE_STRUCTURE_DESCRIPTION(MongoImportConfig);
@@ -169,14 +170,80 @@ struct MongoImportProcedure: public Procedure {
         return result;
     }
 
+    static ExpressionValue bsonToExpression(const bsoncxx::types::value & val,
+                                            Date ts)
+    {
+        switch (val.type()) {
+        case bsoncxx::type::k_undefined:
+        case bsoncxx::type::k_null:
+            return ExpressionValue::null(ts);
+        case bsoncxx::type::k_double:
+            return ExpressionValue(val.get_double().value, ts);
+        case bsoncxx::type::k_utf8:
+            return ExpressionValue(val.get_utf8().value.to_string(), ts);
+        case bsoncxx::type::k_document: {
+            auto doc = val.get_document();
+            return bsonToExpression(doc.view());
+        }
+        case bsoncxx::type::k_binary: {
+            auto bin = val.get_binary();
+            return ExpressionValue(CellValue::blob((const char *)bin.bytes, bin.size),
+                                   ts);
+        }
+        case bsoncxx::type::k_oid:
+            return ExpressionValue(val.get_oid().value.to_string(), ts);
+        case bsoncxx::type::k_bool:
+            return ExpressionValue(val.get_bool().value, ts);
+        case bsoncxx::type::k_date:
+            return ExpressionValue(val.get_date().value, ts);
+        case bsoncxx::type::k_timestamp:
+            return ExpressionValue(val.get_timestamp().timestamp, ts);
+        case bsoncxx::type::k_int32:
+            return ExpressionValue(val.get_int32().value, ts);
+        case bsoncxx::type::k_int64:
+            return ExpressionValue(val.get_int64().value, ts);
+        case bsoncxx::type::k_symbol:
+            return ExpressionValue(val.get_symbol().symbol.to_string(), ts);
+            
+        case bsoncxx::type::k_array:
+            throw HttpReturnException(500, "BSON array conversion not done");
+            
+        case bsoncxx::type::k_regex:
+            throw HttpReturnException(500, "BSON regex conversion not done");
+
+        case bsoncxx::type::k_dbpointer:
+        case bsoncxx::type::k_code:
+        case bsoncxx::type::k_codewscope:
+        case bsoncxx::type::k_maxkey:
+        case bsoncxx::type::k_minkey:
+            throw HttpReturnException(500, "BSON internal conversions not accepted");
+        }
+        
+        throw HttpReturnException(500, "Unknown bson expression type");
+    }
+
+    static ExpressionValue bsonToExpression(const bsoncxx::document::view & doc)
+    {
+        auto ts = Date::fromSecondsSinceEpoch(doc["_oid"].get_oid().value.get_time_t());
+        StructValue cols;
+        for (auto & el: doc) {
+            cols.emplace_back(PathElement(std::move(el.key().to_string())),
+                              bsonToExpression(el.get_value(), ts));
+        }
+        return std::move(cols);
+    }
+
     virtual RunOutput run(const ProcedureRunConfig & run,
                           const std::function<bool (const Json::Value &)> & onProgress) const
     {
-        cerr << "importing mongodb dataset" << endl;
+        const char * uri = getenv("MONGO_DB_URI");
+        if (!uri)
+            uri = "mongodb://localhost:27017";
+        //cerr << "importing mongodb dataset" << endl;
 
-        mongocxx::client conn{mongocxx::uri{"mongodb://localhost:27017"}};
+        mongocxx::client conn{mongocxx::uri{uri}};
 
-        auto db = conn["test"];
+        auto db = conn[""];
 
         cerr << "connected to " << db.name() << endl;
 
@@ -184,6 +251,7 @@ struct MongoImportProcedure: public Procedure {
         {
             auto cursor = db["restaurants"].find({});
             for (auto&& doc : cursor) {
+                auto expr = bsonToExpression(doc);
                 std::cout << bsoncxx::to_json(doc) << std::endl;
             }
         }
@@ -231,7 +299,6 @@ regMongodbDataset(mongodbPackage(),
 
 static RegisterProcedureType<MongoImportProcedure, MongoImportConfig>
 regMongodbImport(mongodbPackage(),
-                 "mongodb.import",
                  "Import a dataset from mongodb",
                  "MongoImport.md.html");
 
