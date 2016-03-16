ad46d8052c741d2ae32468bdb31b477b52640202
Mathieu Bolduc <mbolduc@datacratic.com>
2016-03-16 15:19:57 -0400

    Quick fix for Accuracy procedure thread-safety issue

--- plugins/accuracy.cc
+++ plugins/accuracy.cc
@@ -435,6 +435,7 @@ runRegression(AccuracyConfig & runAccuracyConf,
 
     PerThreadAccumulator<Rows> rowsAccum;
     Date recordDate = Date::now();
+    mutex lineMutex;
 
     auto aggregator = [&] (NamedRowValue & row,
                            const std::vector<ExpressionValue> & scoreLabelWeight)
@@ -454,6 +455,7 @@ runRegression(AccuracyConfig & runAccuracyConf,
 
                 rowsAccum.get().emplace_back(row.rowName, outputRow);
                 if(rowsAccum.get().size() > 1000) {
+                    std::unique_lock<std::mutex> guard(lineMutex);
                     output->recordRows(rowsAccum.get());
                     rowsAccum.get().clear();
                 }
