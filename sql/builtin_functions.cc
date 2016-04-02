4bb42d4e06ddea7e954fd6bbd64709bd44891559
Jeremy Barnes <jeremy@datacratic.com>
2016-04-01 22:44:37 -0400

    Made vector_xxxx functions safe (MLDB-1486)

--- sql/builtin_functions.cc
+++ sql/builtin_functions.cc
@@ -1480,7 +1480,7 @@ BoundFunction date_trunc(const std::vector<BoundSqlExpression> & args)
 
 static RegisterBuiltin registerdate_trunc(date_trunc, "date_trunc");
 
-void normalize(ML::distribution<float>& val, double p)
+void normalize(ML::distribution<double>& val, double p)
 {
     if (p == 0) {
         double n = (val != 0).count();
@@ -1532,7 +1532,8 @@ BoundFunction normalize(const std::vector<BoundSqlExpression> & args)
                  const SqlRowScope & scope) -> ExpressionValue
             {
                     // Get it as an embedding
-                    ML::distribution<float> val = args.at(0).getEmbedding();
+                    ML::distribution<double> val
+                        = args.at(0).getEmbeddingDouble();
                     Date ts = args.at(0).getEffectiveTimestamp();
                     double p = args.at(1).toDouble();
 
@@ -1563,7 +1564,7 @@ BoundFunction normalize(const std::vector<BoundSqlExpression> & args)
                  const SqlRowScope & scope) -> ExpressionValue
             {
                 // Get it as an embedding
-                ML::distribution<float> val = args[0].getEmbedding();
+                ML::distribution<double> val = args[0].getEmbeddingDouble();
                 Date ts = args[0].getEffectiveTimestamp();
                 double p = args[1].toDouble();
 
@@ -1598,7 +1599,7 @@ BoundFunction norm(const std::vector<BoundSqlExpression> & args)
                  const SqlRowScope & scope) -> ExpressionValue
             {
                 // Get it as an embedding
-                ML::distribution<float> val = args[0].getEmbedding();
+                ML::distribution<double> val = args[0].getEmbeddingDouble();
                 Date ts = args[0].getEffectiveTimestamp();
 
                 double p = args[1].toDouble();
@@ -2165,7 +2166,7 @@ struct RegisterVectorOp {
                     std::tie(embedding1, embedding2, token, ts)
                         = extract(args[0], args[1]);
 
-                    return reconst(Op::apply(embedding1, embedding2), token.get(),
+                    return reconst(Op::apply(embedding1, embedding2), token,
                                    ts);
                 },
                 info};
