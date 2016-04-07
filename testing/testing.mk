d1707631827fb623118b7084280a554acef48cfc
Nicolas Kruchten <nicolas@datacratic.com>
2016-04-07 13:18:01 -0400

    failing count({*}) test for MLDB-1507

--- testing/testing.mk
+++ testing/testing.mk
@@ -333,6 +333,7 @@ $(eval $(call mldb_unit_test,ranking_test.py))
 $(eval $(call mldb_unit_test,MLDB-1490-grouped-validation.py))
 $(eval $(call mldb_unit_test,MLDB-1491-get-all-not-implemented-for-datasets.js,,manual)) # awaiting fix
 $(eval $(call mldb_unit_test,MLDB-1500-transpose-query.js,,manual)) # awaiting fix
+$(eval $(call mldb_unit_test,MLDB-1507-groupby.py))
 
 # The MLDB-1398 test case requires a library and a plugin
 # Tensorflow plugins
