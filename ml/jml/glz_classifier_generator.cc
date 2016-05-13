b37b5a3137c409b3bba9107026b74eff35212be9
Mathieu Marquis Bolduc <mbolduc@datacratic.com>
2016-05-13 11:15:03 -0400

    Update glz_classifier_generator.cc

--- ml/jml/glz_classifier_generator.cc
+++ ml/jml/glz_classifier_generator.cc
@@ -338,8 +338,6 @@ train_weighted(Thread_Context & thread_context,
     /* Scale */
     for (unsigned v = 0;  v < nv && normalize;  ++v) {
 
-	std::cerr << "NORMALIZING" << std::endl;
-		
         double total = 0.0;
 
         for (unsigned x = 0;  x < nx2;  ++x)
@@ -364,9 +362,6 @@ train_weighted(Thread_Context & thread_context,
             std = 1.0;
             
         double std_recip = 1.0 / std;
-
-        cerr << "Std/stdr: " << std << "," << std_recip << endl;
-
         for (unsigned x = 0;  x < nx2;  ++x)
             dense_data[v][x] = (dense_data[v][x] - mean) * std_recip;
 
