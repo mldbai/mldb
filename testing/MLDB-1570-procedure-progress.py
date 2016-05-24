10ca5109295933347246bc1c80f80431670293a3
Francois-Michel L Heureux <fmlheureux@datacratic.com>
2016-05-24 16:49:30 -0400

    Cleanup test

--- testing/MLDB-1570-procedure-progress.py
+++ testing/MLDB-1570-procedure-progress.py
@@ -1,5 +1,5 @@
 #
-# MLDB-XXXX-procedure-progress.py
+# MLDB-1570-procedure-progress.py
 # 2016-03-30
 # This file is part of MLDB. Copyright 2016 Datacratic. All rights reserved.
 #
@@ -36,7 +36,12 @@ class ProcedureProgressTest(MldbUnitTest):
                     'id' : 'output',
                     'type' : 'sparse.mutable'
                 },
-                'percentileBuckets': {'b1': [0, 25], 'b2': [25, 50], 'b3': [50, 75], 'b4': [75, 100]}
+                'percentileBuckets': {
+                    'b1': [0, 25],
+                    'b2': [25, 50],
+                    'b3': [50, 75],
+                    'b4': [75, 100]
+                }
             }
         })
 
@@ -44,14 +49,21 @@ class ProcedureProgressTest(MldbUnitTest):
         last_percent = 0.0
         while(running):
             resp = mldb.get(location).json()
-            assert 'id' in resp, "status is expected to return the id of the run"
-            assert 'state' in resp, "status is expected to return the state of the run"
+            self.assertTrue('id' in resp,
+                            "status is expected to return the id of the run")
+            self.assertTrue(
+                'state' in resp,
+                "status is expected to return the state of the run")
             if resp['state'] == 'finished':
                 running = False
             elif resp['state'] == 'executing': # still executing
-                assert 'progress' in resp, "status is expected to return the progress of the run " + str(resp)
-                current_percent = resp['progress']['steps'][0]['percent'] 
-                assert current_percent >= last_percent, 'percent must be increasing'
+                self.assertTrue(
+                    'progress' in resp,
+                    "status is expected to return the progress of the run "
+                    + str(resp))
+                current_percent = resp['progress']['steps'][0]['percent']
+                self.assertGreaterEqual(current_percent, last_percent,
+                                        'percent must be increasing')
                 last_percent = current_percent
                 mldb.log(resp)
             time.sleep(0.001)
@@ -71,4 +83,6 @@ class ProcedureProgressTest(MldbUnitTest):
 
         resp = mldb.post("/v1/procedures/test/runs")
         mldb.log(resp)
-mldb.run_tests()
+
+if __name__ == '__main__':
+    mldb.run_tests()
