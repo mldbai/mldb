a975b458a91915cf79233818c50d4072d2d36777
Francois-Michel L Heureux <fmlheureux@datacratic.com>
2016-06-07 16:57:46 -0400

    Added classifier err on empty sets test

---
+++ testing/classifier_err_on_empty_sets_test.py
@@ -0,0 +1,116 @@
+#
+# classifier_err_on_empty_sets.py
+# Mich, 2016-06-07
+# This file is part of MLDB. Copyright 2016 Datacratic. All rights reserved.
+#
+if False:
+    mldb_wrapper = None
+mldb = mldb_wrapper.wrap(mldb)  # noqa
+
+class ClassifierErrorWhenNoDataTest(MldbUnitTest):  # noqa
+
+    @classmethod
+    def setUpClass(cls):
+        ds = mldb.create_dataset({'id' : 'ds', 'type' : 'sparse.mutable'})
+        ds.record_row('row1', [['label', 1, 12], ['feat1', 1, 0], ['feat2', 1, 0]])
+        ds.record_row('row2', [['label', 0, 12], ['feat1', 1, 0], ['feat2', 0, 0]])
+        ds.record_row('row3', [['label', 0, 12], ['feat1', 0, 0], ['feat2', 0, 0]])
+        ds.commit()
+
+        cls.classifier_train("""
+                SELECT {* EXCLUDING(label)} AS features, label
+                FROM ds
+        """)
+
+    @classmethod
+    def classifier_train(cls, training_data):
+        mldb.post('/v1/procedures', {
+            'type' : 'classifier.train',
+            'params' : {
+                'runOnCreation' : True,
+                "mode": "boolean",
+                'algorithm' : 'glz',
+                "configuration": {
+                    "glz": {
+                        "type": "glz",
+                        "verbosity": 3,
+                        "normalize": False,
+                        "link": "linear",
+                        "regularization": 'l2'
+                    }
+                },
+                'trainingData' : training_data,
+                "modelFileUrl":
+                    "file://build/x86_64/tmp/fmlhTODO.cls",
+            }
+        })
+
+    def test_classifier_train_no_data(self):
+        err_str = 'Training set is empty'
+        with self.assertRaisesRegexp(mldb_wrapper.ResponseException, err_str):
+            self.classifier_train("""
+                SELECT {* EXCLUDING(label)} AS features, label
+                FROM ds LIMIT 0""")
+
+        with self.assertRaisesRegexp(mldb_wrapper.ResponseException, err_str):
+            self.classifier_train("""
+                SELECT {* EXCLUDING(label)} AS features, label
+                FROM ds OFFSET 10""")
+
+        with self.assertRaisesRegexp(mldb_wrapper.ResponseException, err_str):
+            self.classifier_train("""
+                SELECT {* EXCLUDING(label)} AS features, label
+                FROM ds WHERE whatever=123""")
+
+    def test_classifier_test_no_data(self):
+        err_str = 'Testing set is empty'
+        with self.assertRaisesRegexp(mldb_wrapper.ResponseException, err_str):
+            mldb.post('/v1/procedures', {
+                "type": "classifier.test",
+                "params": {
+                    'runOnCreation' : True,
+                    "testingData": """
+                        SELECT
+                            {* EXCLUDING(label)} AS features,
+                            label AS score,
+                            label AS label
+                        FROM ds
+                        LIMIT 0
+                    """
+                }
+            })
+
+        with self.assertRaisesRegexp(mldb_wrapper.ResponseException, err_str):
+            mldb.post('/v1/procedures', {
+                "type": "classifier.test",
+                "params": {
+                    'runOnCreation' : True,
+                    "testingData": """
+                        SELECT
+                            {* EXCLUDING(label)} AS features,
+                            label AS score,
+                            label AS label
+                        FROM ds
+                        OFFSET 100
+                    """
+                }
+            })
+
+        with self.assertRaisesRegexp(mldb_wrapper.ResponseException, err_str):
+            mldb.post('/v1/procedures', {
+                "type": "classifier.test",
+                "params": {
+                    'runOnCreation' : True,
+                    "testingData": """
+                        SELECT
+                            {* EXCLUDING(label)} AS features,
+                            label AS score,
+                            label AS label
+                        FROM ds
+                        WHERE patate=123
+                    """
+                }
+            })
+
+if __name__ == '__main__':
+    mldb.run_tests()
