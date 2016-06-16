34b0220f3111aa7ff2a9d2b432cd74fa36395026
Francois Maillet <francois@recoset.com>
2016-06-16 18:57:04 -0400

    [MLDB-1732] Added failing test case

---
+++ testing/MLDB-1732-columnCount_in_where.py
@@ -0,0 +1,34 @@
+#
+# MLDB-1732-columnCount_in_where.py
+# 16 juin 2016, Francois Maillet
+# This file is part of MLDB. Copyright 2016 Datacratic. All rights reserved.
+#
+
+import unittest
+
+mldb = mldb_wrapper.wrap(mldb) # noqa
+
+class Mldb1732(MldbUnitTest):
+    @classmethod
+    def setUpClass(self):
+        ds = mldb.create_dataset({ "id": "sample", "type": "sparse.mutable" })
+        ds.record_row("a",[["x", 1, 0]])
+        ds.record_row("b",[])
+        ds.commit()
+
+    def test_no_table_error_message(self):
+        with self.assertRaisesRegexp(mldb_wrapper.ResponseException,
+                'FROM statement is missing'):
+            mldb.query("select * where columnCount() > 0")
+
+    def test_where_columncount_works(self):
+        self.assertTableResultEquals(
+            mldb.query("select * from sample where columnCount() > 0"),
+            [
+                ["_rowName", "x"],
+                [       "a",  1 ]
+            ]
+        )
+
+mldb.run_tests()
+
