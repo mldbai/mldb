5b8970902c3ac47f94985e63f6796f7a411c31be
Jeremy Barnes <jeremy@datacratic.com>
2016-06-24 16:50:05 -0400

    Another clang fix (TRIVIAL)

--- ml/jml/naive_bayes_generator.h
+++ ml/jml/naive_bayes_generator.h
@@ -57,7 +57,7 @@ public:
              const Training_Data & validation_data,
              const distribution<float> & training_weights,
              const distribution<float> & validation_weights,
-             const std::vector<Feature> & features, int) const;
+             const std::vector<Feature> & features, int) const override;
 
     int trace;
     float feature_prop;
