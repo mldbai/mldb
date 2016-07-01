e6c623e2a68bd012f61cf3bcc063dbdfc1405384
Francois Maillet <francois@recoset.com>
2016-07-01 13:14:34 -0400

    [MLDB-1771] Fix label weighting for categorical training

--- plugins/classifier.cc
+++ plugins/classifier.cc
@@ -530,9 +530,26 @@ run(const ProcedureRunConfig & run,
 
     ML::Training_Data trainingSet(featureSpace);
 
-    ML::distribution<float> labelWeights[2];
-    labelWeights[0].resize(nx);
-    labelWeights[1].resize(nx);
+
+    unsigned num_weight_labels;
+    switch (runProcConf.mode) {
+    case CM_REGRESSION:
+        num_weight_labels = 1;
+        break;
+    case CM_BOOLEAN:
+        num_weight_labels = 2;
+        break;
+    case CM_CATEGORICAL:
+        num_weight_labels = labelMapping.size();
+        break;
+    default:
+        throw HttpReturnException(400, "Unknown classifier mode");
+    }
+
+    std::vector<ML::distribution<float>> labelWeights(num_weight_labels);
+    for(int i=0; i<num_weight_labels; i++) {
+        labelWeights[i].resize(nx);
+    }
 
     ML::distribution<float> exampleWeights(nx);
 
@@ -556,8 +573,12 @@ run(const ProcedureRunConfig & run,
 
         trainingSet.add_example(std::make_shared<ML::Mutable_Feature_Set>(std::move(fvs[i].featureSet)));
 
-        labelWeights[0][i] = weight * !label;
-        labelWeights[1][i] = weight * label;
+        if(runProcConf.mode != CM_REGRESSION) {
+            for(int lbl=0; lbl<num_weight_labels; lbl++) {
+                labelWeights[lbl][i] = weight * (label == lbl);
+            }
+        }
+
         exampleWeights[i]  = weight;
     }
 
@@ -633,16 +654,17 @@ run(const ProcedureRunConfig & run,
         weights = exampleWeights;
     }
     else {
-        double factorTrue  = pow(labelWeights[1].total(), -equalizationFactor);
-        double factorFalse = pow(labelWeights[0].total(), -equalizationFactor);
+        ML::distribution<float> factor_accum(exampleWeights.size(), 0);
+        for(int lbl=0; lbl<num_weight_labels; lbl++) {
+            double factor = pow(labelWeights[lbl].total(), -equalizationFactor);
 
-        INFO_MSG(logger) << "factorTrue = " << factorTrue;
-        INFO_MSG(logger) << "factorFalse = " << factorFalse;
+            //INFO_MSG(logger) 
+            cerr << "factor for class " << lbl << " = " << factor << endl;
 
-        weights = exampleWeights
-            * (factorTrue  * labelWeights[true]
-            + factorFalse * labelWeights[false]);
+            factor_accum += factor * labelWeights[lbl];
+        }
 
+        weights = exampleWeights * factor_accum;
         weights.normalize();
     }
 
--- testing/MLDB-1766_dt_categorical_iris.py
+++ testing/MLDB-1766_dt_categorical_iris.py
@@ -4,6 +4,9 @@
 # This file is part of MLDB. Copyright 2016 Datacratic. All rights reserved.
 #
 
+from datetime import datetime
+from random import random, gauss
+
 mldb = mldb_wrapper.wrap(mldb)  # noqa
 
 class MLDB1766DtCategoricalIris(MldbUnitTest):  # noqa
@@ -19,7 +22,34 @@ class MLDB1766DtCategoricalIris(MldbUnitTest):  # noqa
             }
         })
 
-    def test_it(self):
+        ## create unbalanced dataset
+        dataset_config = {
+            'type'    : 'sparse.mutable',
+            'id'      : "cat_weights"
+        }
+
+        dataset = mldb.create_dataset(dataset_config)
+        now = datetime.now()
+
+        for label in ["a", "b"]:
+            for i in xrange(5000):
+                dataset.record_row("u%d-%s" % (i, label), 
+                        [["feat1", gauss(5 if label == "a" else 15, 3), now],
+                         ["feat2", gauss(-5 if label == "a"  else 10, 10), now],
+                         ["feat3", gauss(0, 10), now],
+                         ["label", label, now]])
+
+        for i in xrange(500):
+            dataset.record_row("u%d-c" % i, 
+                        [["feat1", gauss(10, 5), now],
+                         ["feat2", gauss(0, 10), now],
+                         ["feat3", gauss(5, 10), now],
+                         ["label", "c", now]])
+
+        dataset.commit()
+
+
+    def test_not_perfect_performance(self):
         mldb.put('/v1/procedures/iris_train_classifier', {
             'type' : 'classifier.train',
             'params' : {
@@ -39,7 +69,7 @@ class MLDB1766DtCategoricalIris(MldbUnitTest):  # noqa
                         "update_alg": "prob"
                     },
                 },
-                "modelFileUrl": "file://models/iris.cls",
+                "modelFileUrl": "file://tmp/iris.cls",
                 "mode": "categorical",
                 "functionName": "iris_classify",
                 "runOnCreation": True
@@ -66,6 +96,82 @@ class MLDB1766DtCategoricalIris(MldbUnitTest):  # noqa
         runResults = rez.json()["status"]["firstRun"]["status"]
 
         self.assertLess(runResults["labelStatistics"]["Iris-virginica"]["recall"], 0.95)
+
+    
+    def test_boolean_unbalanced(self):
+        ## create new dataset without label = b
+        mldb.put("/v1/procedures/bumblebee", {
+            "type": "transform",
+            "params": {
+                "inputData": """
+                    select feat*, label ='a' as label 
+                    from cat_weights
+                    where label != 'b'
+                """,
+                "outputDataset": "boolean_unbalanced",
+                "runOnCreation": True
+            }
+        })
+
+        rez = mldb.put("/v1/procedures/cat_weights", {
+            "type": "classifier.experiment",
+            "params": {
+                "experimentName": "cat_weights",
+                "mode": "boolean",
+                "inputData": """
+                    select 
+                        {feat*} as features,
+                        label
+                    from boolean_unbalanced
+                """,
+                "algorithm": "dt",
+                "configuration": {
+                    "dt": {
+                        "type": "decision_tree",
+                        "max_depth": 8,
+                        "verbosity": 3,
+                        "update_alg": "prob"
+                    },
+                },
+                "modelFileUrlPattern": "file://tmp/cat_weights.cls",
+                "runOnCreation": True
+            }
+        })
+
+        runResults = rez.json()["status"]["firstRun"]["status"]["folds"][0]["resultsTest"]
+        self.assertGreater(runResults["auc"], 0.68)
+
+    def test_categorical_unbalanced(self):
+        rez = mldb.put("/v1/procedures/cat_weights", {
+            "type": "classifier.experiment",
+            "params": {
+                "experimentName": "cat_weights",
+                "mode": "categorical",
+                "inputData": """
+                    select 
+                        {feat*} as features,
+                        label
+                    from cat_weights
+                """,
+                "algorithm": "dt",
+                "equalizationFactor": 1,
+                "configuration": {
+                    "dt": {
+                        "type": "decision_tree",
+                        "max_depth": 10,
+                        "verbosity": 3,
+                        "update_alg": "prob"
+                    },
+                },
+                "modelFileUrlPattern": "file://tmp/cat_weights.cls",
+                "runOnCreation": True
+            }
+        })
+
+        runResults = rez.json()["status"]["firstRun"]["status"]["folds"][0]["resultsTest"]
+        self.assertGreater(runResults["labelStatistics"]["c"]["accuracy"], 0.2)
+
+
 
 if __name__ == '__main__':
     mldb.run_tests()
