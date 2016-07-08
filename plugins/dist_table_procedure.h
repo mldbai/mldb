c2fc7404aa3ceee5fa9cd755927e6dd2382f7aa8
Francois Maillet <francois@recoset.com>
2016-07-08 15:45:13 -0400

    [MLDB-1790] Headers

--- plugins/dist_table_procedure.h
+++ plugins/dist_table_procedure.h
@@ -1,8 +1,6 @@
-// This file is part of MLDB. Copyright 2015 Datacratic. All rights reserved.
-
 /** dist_table_procedure.h                                         -*- C++ -*-
     Simon Lemieux, June 2015
-    Copyright (c) 2015 Datacratic Inc.  All rights reserved.
+    This file is part of MLDB. Copyright 2015 Datacratic. All rights reserved.
 
     distTable procedure
 */
