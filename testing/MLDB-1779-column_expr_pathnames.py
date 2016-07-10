1bf8473d2a0738a6600e9236b0499a08b2444cfe
Francois Maillet <francois@recoset.com>
2016-07-10 17:22:19 -0400

    [MLDBFB-618] Calling {row|column}PathElement() with an index bigger than the length of the path will simply return NULL instead of throwing

---
+++ testing/MLDB-1779-column_expr_pathnames.py
@@ -0,0 +1,42 @@
+#
+# MLDB-1779_column_expr.py
+# Francois Maillet, 2016-07-06
+# This file is part of MLDB. Copyright 2016 Datacratic. All rights reserved.
+#
+
+mldb = mldb_wrapper.wrap(mldb)  # noqa
+
+class MLDB1779ColumnExpr(MldbUnitTest):  # noqa
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    def test_it(self):
+        self.assertTableResultEquals(
+            mldb.query("SELECT column expr () from (select x.a:1, y.b:2)"),
+            mldb.query("SELECT * from (select x.a:1, y.b:2)"))
+
+    def test_columnPathElement(self):
+        self.assertTableResultEquals(
+            mldb.query("""
+                select COLUMN EXPR (WHERE columnPathElement(1) = '0')
+                FROM (
+                    select parse_json('{"age": 5, "friends": [{"name": "tommy"}, {"name": "sally"}]}') as *
+                )
+            """),
+            [
+                [
+                    "_rowName",
+                    "friends.0.name"
+                ],
+                [
+                    "result",
+                    "tommy"
+                ]
+            ])
+
+
+if __name__ == '__main__':
+    mldb.run_tests()
+
