e0a58d1f57a737956594bd2442b0e9e3b8a85b6e
Francois-Michel L Heureux <fmlheureux@datacratic.com>
2016-07-15 11:24:35 -0400

    MLDB-1806 stddev test random sequences

--- testing/stddev_builtin_fct_test.py
+++ testing/stddev_builtin_fct_test.py
@@ -1,5 +1,5 @@
 #
-# std_dev_builtin_fct_test.py
+# stddev_builtin_fct_test.py
 # Francois-Michel L Heureux, 2016-07-13
 # This file is part of MLDB. Copyright 2016 Datacratic. All rights reserved.
 #
@@ -30,28 +30,6 @@ class StdDevBuiltinFctTest(MldbUnitTest):  # noqa
         mldb_res = mldb.query("SELECT vertical_stddev(a) FROM ds")[1][1]
         self.assertAlmostEqual(mldb_res, float_res)
 
-    @unittest.expectedFailure # MLDB-1808
-    def test_random_sequences(self):
-        """
-        Generate random sequences and compare the result with numpy.
-        """
-        for size in xrange(2, 100):
-            ds = mldb.create_dataset({'id' : 'rand', 'type' : 'tabular'})
-            sequence = []
-            for row in xrange(size):
-                sequence.append(random.random() * 1000000)
-                ds.record_row(row, [['a', sequence[-1], 0]])
-            ds.commit()
-
-            mldb.log(sequence)
-            mldb_res = mldb.query("SELECT stddev(a) FROM rand")[1][1]
-            numpy_res = float(numpy.var(sequence, ddof=1))
-            mldb.log("mldb:  {}".format(mldb_res))
-            mldb.log("numpy: {}".format(numpy_res))
-            self.assertAlmostEqual(mldb_res, numpy_res)
-            ds.delete()
-            mldb.log("Success with a size of {}".format(size))
-
     def test_nan(self):
         ds = mldb.create_dataset({'id' : 'null_ds', 'type' : 'tabular'})
         ds.record_row('1', [['a', 1, 0]])
@@ -62,24 +40,41 @@ class StdDevBuiltinFctTest(MldbUnitTest):  # noqa
         res = mldb.query("SELECT stddev(c) FROM null_ds")
         self.assertEqual(res[1][1], "NaN")
 
-    def test_some_numbers(self):
-        ds = mldb.create_dataset({'id' : 'some_numbers', 'type' : 'tabular'})
-        number_1 = 71218.50311678024
-        number_2 = 255650.6226198759
-        arr = ['a', number_1, 0]
-        mldb.log(arr)
-        ds.record_row('1', [arr])
-        ds.record_row('2', [['a', number_2, 0]])
-        ds.commit()
-        res = mldb.query("SELECT stddev(a) FROM some_numbers")
-        mldb.log(res)
+    def test_random_sequences(self):
+        """
+        Generate random sequences and compare the result with numpy.
+        """
+        lines = [""]
+        lines.append(
+            '| {:^4} | {:^20} | {:^20} | {:^20} | {:^10} | {:^3} |'
+            .format('size', 'MLDB', 'Numpy', 'Diff (%)', 'Diff', 'Err'))
+        err_cnt = 0
+        for size in xrange(2, 100):
+            ds = mldb.create_dataset({'id' : 'rand', 'type' : 'tabular'})
+            sequence = []
+            for row in xrange(size):
+                sequence.append(random.random() * 1000000)
+                ds.record_row(row, [['a', sequence[-1], 0]])
+            ds.commit()
 
-        res = mldb.query("SELECT a FROM some_numbers ORDER BY rowName()")
-        self.assertTableResultEquals(res, [
-            ['_rowName', 'a'],
-            ['1', number_1],
-            ['2', number_2]
-        ])
+            mldb_res = mldb.query("SELECT stddev(a) FROM rand")[1][1]
+            numpy_res = float(numpy.var(sequence, ddof=1))
+            if (numpy == 0):
+                mldb.log("Skipping case where numpy_re == 0")
+            else:
+                rx = abs(mldb_res - numpy_res) / numpy_res * 100
+                err = ''
+                if rx > 0.0000000000001:
+                    err_cnt += 1
+                    err = 'ERR'
+                    mldb.log(sequence)
+                lines.append(
+                    '| {:>4} | {:>20f} | {:>20f} | {:>10.18f} | {:>10f} | {:^3} |'
+                    .format(size, mldb_res, numpy_res, rx,
+                            abs(mldb_res - numpy_res), err))
+            mldb.delete('/v1/datasets/rand')
+        mldb.log('\n'.join(lines))
+        mldb.log(err_cnt)
 
 if __name__ == '__main__':
     mldb.run_tests()
