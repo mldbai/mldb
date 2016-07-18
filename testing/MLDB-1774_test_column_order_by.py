91de1965f47e01377d670e58bbf75926704c1cc8
Francois Maillet <francois@recoset.com>
2016-07-18 15:53:15 -0400

    [MLDB-1774] Removed some test cases

--- testing/MLDB-1774_test_column_order_by.py
+++ testing/MLDB-1774_test_column_order_by.py
@@ -65,44 +65,6 @@ class MLDB1774TestColumnOrderBy(MldbUnitTest):  # noqa
 		self.assertEqual(cmp(col_list2[1:6], expected), 0);
 
 
-	# testing different ways to order columns using TRANSPOSE and ROW ordering operations
-
-	def test_transpose_column_expr_rowCount(self):
-		dt3  = mldb.query("""
-			SELECT rowName() 
-			FROM transpose((
-				SELECT COLUMN EXPR (AS columnName() ORDER BY rowCount() DESC) FROM ml_concepts
-			))
-			LIMIT 5
-		""")
-
-		col_list3 = []
-		for i in xrange (1, len(dt3)):
-			col_list3.append(dt3[i][1])
-
-		expected = ['with', 'learning', 'machine', 'neural', 'model']
-
-		#mldb.log(col_list3)
-		self.assertEqual(cmp(col_list3, expected), 0);
-
-
-	def test_transpose_order_vertical_rowCount(self):
-		dt4 = mldb.query("""
-			SELECT rowName() from transpose(
-				(SELECT vertical_count({*}) as * NAMED 'num' FROM ml_concepts)
-			)
-			ORDER BY num DESC
-			LIMIT 5
-		""")
-
-		col_list4 = []
-		for i in xrange (1, len(dt4)):
-			col_list4.append(dt4[i][1])
-		
-		expected = ['with', 'learning', 'machine', 'neural', 'model']
-
-		self.assertEqual(cmp(col_list4, expected), 0);
-
 
 if __name__ == '__main__':
 	mldb.run_tests()
