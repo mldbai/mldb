b5faeaab6a3a582bcab879ec658875966f7f38f3
Francois-Michel L Heureux <fmlheureux@datacratic.com>
2016-08-02 14:50:47 -0400

    Plugin autoload test

--- mongodb/MLDB-1413-mongodb-plugin.js
+++ mongodb/MLDB-1413-mongodb-plugin.js
@@ -1,6 +1,8 @@
 
-var resp = mldb.put('/v1/procedures/import', {type: 'mongodb.import', params: { runOnCreation: true} });
-
+var resp = mldb.put('/v1/procedures/import', {type: 'mongodb.import'});
+if (resp["json"]["error"] != "failed to create the initial run") {
+    throw "Failed to load plugin";
+}
 mldb.log(resp);
 
 "success"
