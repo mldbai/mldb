4289bde9b7e37cd3498f22514b560e0c2ee79d94
Jean Raby <jean@datacratic.com>
2016-08-10 18:08:00 -0400

    Use a better regex to match possible leading dir

--- jml-build/get_git_revision.sh
+++ jml-build/get_git_revision.sh
@@ -86,11 +86,12 @@ if [ $NUMUNCLEAN -gt 0 ]; then
             SMFILESUSED=$FILESUSED
         else
             echo "doing submodule $sm" >> /dev/stderr
-            SMFILESUSED=`pwd`/$BUILD/tmp/files-used-$target-submodule-$(echo $1 | tr '/' '-')
+            SMCLEANEDNAME=$(echo $sm | tr '/' '-')
+            SMFILESUSED=`pwd`/$BUILD/tmp/files-used-$target-submodule-$SMCLEANEDNAME
             pushd $sm > /dev/null
             echo -n "extracting build files used in dirty submodule $sm..." >> /dev/stderr
             # Allow a leading directory to show up in the submodule path.
-            cat $FILESUSED | grep -E "^[a-zA-Z0-9]*/$sm/" | sed "s!^.*$sm/!!g" | sort > $SMFILESUSED
+            cat $FILESUSED | grep -E "^([^/]*/)?$sm/" | sed "s!^.*$sm/!!g" | sort > $SMFILESUSED
         fi
 
         cat $SMFILESUSED | wc -l > /dev/stderr
