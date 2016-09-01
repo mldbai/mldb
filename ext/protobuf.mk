265d3e56acc8f8fe6eb2e012f4c35bd1a00b62ea
Jeremy Barnes <jeremy@datacratic.com>
2016-09-01 10:45:51 -0400

    Made protobuf build more user-friendly (MLDB-1903)

--- ext/protobuf.mk
+++ ext/protobuf.mk
@@ -3,7 +3,7 @@
 # run on the HOST, as they are a build dependency, not on the target.
 #
 # We use a recursive make, since we don't care about anything apart from the
-# final executable.
+# final executable, the libraries, and the headers.
 
 ifneq ($(PREMAKE),1)
 
@@ -20,33 +20,27 @@ define build_protobuf_for_arch
 PROTOC_EXTRA_ARGS_$(1):=$(if $(call sne,$(1),$(HOSTARCH)),--with-protoc=$(PWD)/$(HOSTBIN)/protoc --host=$(1)-linux-gnu --target=$(1)-linux-gnu)$(if $(call seq,$(1),$(ARCH)), CC="$(CC)" CXX="$(CXX)")
 
 $(4)/protoc: $(if $(call sne,$(1),$(HOSTARCH)),$(HOSTBIN)/protoc)
-	mkdir -p $(BUILD)/$(1)/tmp/protobuf-build
-	cp -rf mldb/ext/protobuf/* $(BUILD)/$(1)/tmp/protobuf-build
-	cd $(BUILD)/$(1)/tmp/protobuf-build \
-	&& ./autogen.sh \
+	@echo "   $(COLOR_BLUE)[COPY EXTERN]$(COLOR_RESET)                      	protobuf3 $(1)"
+	@mkdir -p $(BUILD)/$(1)/tmp/protobuf-build
+	@cp -rf mldb/ext/protobuf/* $(BUILD)/$(1)/tmp/protobuf-build
+	@echo " $(COLOR_BLUE)[CONFIG EXTERN]$(COLOR_RESET)                      	protobuf3 $(1)"
+	@(cd $(BUILD)/$(1)/tmp/protobuf-build \
+	&& ./autogen.sh > configure-log.txt 2>&1 \
 	&& ./configure \
 		--prefix $(PWD)/$(BUILD)/$(1) \
 		--program-suffix="" \
-		$$(PROTOC_EXTRA_ARGS_$(1))
-	+$(MAKE) -j -C $(BUILD)/$(1)/tmp/protobuf-build
-	$(MAKE) -j install -C $(BUILD)/$(1)/tmp/protobuf-build
+		$$(PROTOC_EXTRA_ARGS_$(1)) >> configure-log.txt 2>&1) \
+	|| (echo $(COLOR_RED)Protobuf configure failed for $(1)$(COLOR_RESET) && cat $(BUILD)/$(1)/tmp/protobuf-build/configure-log.txt && false)
+	@echo "   $(COLOR_BLUE)[MAKE EXTERN]$(COLOR_RESET)                      	protobuf3 $(1)"
+	@+$(MAKE) -j -C $(BUILD)/$(1)/tmp/protobuf-build > $(BUILD)/$(1)/tmp/protobuf-build/make-log.txt 2>&1 || (echo $(COLOR_RED)Protobuf compile failed for $(1)$(COLOR_RESET) && cat $(BUILD)/$(1)/tmp/protobuf-build/compile-log.txt && false)
+	@echo "$(COLOR_BLUE)[INSTALL EXTERN]$(COLOR_RESET)                      	protobuf3 $(1)"
+	@$(MAKE) -j install -C $(BUILD)/$(1)/tmp/protobuf-build > $(BUILD)/$(1)/tmp/protobuf-build/install-log.txt 2>&1 || (echo $(COLOR_RED)Protobuf install failed for $(1)$(COLOR_RESET) && cat $(BUILD)/$(1)/tmp/protobuf-build/install-log.txt && false)
+	@echo "   $(COLOR_BLUE)[DONE EXTERN]$(COLOR_RESET)                      	protobuf3 $(1)"
 
+# we call it libprotobuf3.so to avoid clashing with the system one
 $(2)/libprotobuf3.so:	$(4)/protoc
 	$(if $(call sne,$(2),$(BUILD)/$(1)/lib),cp $(PWD)/$(BUILD)/$(1)/lib/libprotobuf* $(2))
-	cp $(BUILD)/$(1)/lib/libprotobuf.so.10.0.0 $$@~ && mv $$@~ $$@
-
-#		--libdir=$(PWD)/$(2) \
-#		--includedir=$(PWD)/$(3) \
-#		--bindir=$(PWD)/$(4) \
-
-# We use the protobuf library at runtime, so it needs to be installed in
-# the right place.  Here we also rename it to avoid clashes with the system
-# version.
-#$(2)/libprotobuf3.so:	$(HOSTBIN)/protoc
-#	@cp $(HOSTLIB)/libprotobuf.so $@
-
-# We also need to make sure that the header files go in the right place
-#$(INC)/google/protobuf: $(HOSTBIN)/protoc
+	@cp $(BUILD)/$(1)/lib/libprotobuf.so.10.0.0 $$@~ && mv $$@~ $$@
 
 protobuf: $(4)/protoc
 
@@ -57,15 +51,7 @@ $(eval $(call build_protobuf_for_arch,$(HOSTARCH),$(BUILD)/$(HOSTARCH)/lib,$(BUI
 endif
 $(eval $(call build_protobuf_for_arch,$(ARCH),$(LIB),$(INC),$(BIN)))
 
-#else
-#$(LIB)/libprotobuf3.so:	$(HOSTBIN)/protoc#
-#	@cp $(HOSTLIB)/libprotobuf.so $@
-#	@cp $(HOSTLIB)/libprotobuf.so.* $(LIB)
-
-#$(INC)/google/protobuf:	$(HOSTBIN)/protoc
-#	@mkdir -p $(INC)/google && ln -sf $(PWD)/$(HOSTINC)/google/protobuf $@
-#endif
-
-#protobuf: $(HOSTBIN)/protoc-$(ARCH) $(INC)/google/protobuf $(LIB)/libprotobuf3.so
+# Allow a dependency on the headers
+$(INC)/google/protobuf: $(BIN)/protoc
 
 endif
