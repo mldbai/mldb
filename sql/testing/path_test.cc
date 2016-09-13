9358bc88aa76b2a9eb4d8a9399873b4065005048
Jeremy Barnes <jeremy@datacratic.com>
2016-09-13 13:14:09 +0000

    Fixed path ordering with unicode and digits (MLDB-1936)

--- sql/testing/path_test.cc
+++ sql/testing/path_test.cc
@@ -10,6 +10,7 @@
 #include "mldb/sql/path.h"
 #include "mldb/arch/exception_handler.h"
 #include "mldb/types/value_description.h"
+#include "mldb/types/vector_description.h"
 #include "mldb/http/http_exception.h"
 #include "mldb/vfs/filter_streams.h"
 #include <set>
@@ -365,21 +366,28 @@ BOOST_AUTO_TEST_CASE(test_path_builder)
     Path path = builder.extract();
 }
 
-#if 0
+// MLDB-1936
 BOOST_AUTO_TEST_CASE(test_ordered2)
 {
     vector<Path> paths;
 
-    filter_istream stream("columns.txt");
+    vector<int> lengthBuckets;
+
+    filter_istream stream("mldb/sql/testing/path_test_columns.txt");
     while (stream) {
         std::string s;
         getline(stream, s);
         if (s.empty())
             continue;
         paths.emplace_back(Path::parse(s));
+        int l = paths.back().size();
+        if (l >= lengthBuckets.size())
+            lengthBuckets.resize(l + 1);
+        lengthBuckets[l] += 1;
     }
    
     cerr << "got " << paths.size() << " paths" << endl;
+    cerr << jsonEncodeStr(lengthBuckets) << endl;
 
     for (unsigned i = 0;  i < 10;  ++i) {
         std::random_shuffle(paths.begin(), paths.end());
@@ -389,19 +397,31 @@ BOOST_AUTO_TEST_CASE(test_ordered2)
         std::set<uint64_t> orderedHashes;
 
         for (const Path & p: paths) {
+            BOOST_CHECK(!ordered.count(p));
+            BOOST_CHECK(!unordered.count(p));
             BOOST_CHECK(unordered.insert(p).second);
             BOOST_CHECK(unorderedHashes.insert(p.hash()).second);
             BOOST_CHECK(ordered.insert(p).second);
             BOOST_CHECK(orderedHashes.insert(p.hash()).second);
+            BOOST_CHECK(ordered.count(p));
+            BOOST_CHECK(unordered.count(p));
         }
 
         BOOST_CHECK_EQUAL(ordered.size(), paths.size());
         BOOST_CHECK_EQUAL(unordered.size(), paths.size());
         BOOST_CHECK_EQUAL(orderedHashes.size(), paths.size());
         BOOST_CHECK_EQUAL(unorderedHashes.size(), paths.size());
+
+        for (const Path & p: paths) {
+            if (!ordered.count(p))
+                cerr << "missing path " << p << endl;
+            BOOST_CHECK(ordered.count(p));
+            BOOST_CHECK(unordered.count(p));
+            BOOST_CHECK(orderedHashes.count(p.hash()));
+            BOOST_CHECK(unorderedHashes.count(p.hash()));
+        }
     }
 }
-#endif
 
 BOOST_AUTO_TEST_CASE(test_null)
 {
