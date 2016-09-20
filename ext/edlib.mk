9d1a7b1f084e50f3d772e75c7a29ba0f6a471626
François-Michel L'Heureux <fmlheureux@datacratic.com>
2016-09-20 14:19:13 +0000

    Fix edlib + jpeg

--- ext/edlib.mk
+++ ext/edlib.mk
@@ -6,7 +6,7 @@ $(eval $(call library,edlib,$(EDLIB_CC_FILES)))
 # Hacky since the test is within a "main", doesn't use boost and is in a cpp
 # file rather than a cc.
 EDLIB_TEST_FILE:=test/runTests.cpp
-$(eval $(call set_compile_option,$(EDLIB_TEST_FILE),-I ext/edlib/edlib/include))
+$(eval $(call set_compile_option,$(EDLIB_TEST_FILE),-I mldb/ext/edlib/edlib/include))
 $(eval $(call program,edlib_test,edlib,$(EDLIB_TEST_FILE)))
 
 ifneq ($(PREMAKE),1)
