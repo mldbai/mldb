1f97650972337a18da797627cbd9d307169c8d9a
Jeremy Barnes <jeremy@datacratic.com>
2016-09-26 18:59:18 +0000

    Added HTML plugin (MLDB-1162)

---
+++ html/html_plugin.h
@@ -0,0 +1,20 @@
+/** html_plugin.h                                                   -*- C++ -*-
+    Jeremy Barnes, 24 November 2015
+    Copyright (c) 2015 Datacratic Inc.  All rights reserved.
+
+    "Html" plugins for MLDB.
+*/
+
+#pragma once
+
+#include "mldb/core/mldb_entity.h"
+
+namespace MLDB {
+
+/** Package that we register the html types under. */
+const Package & htmlPackage();
+
+} // namespace MLDB
+
+
+
