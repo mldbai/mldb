601641a684ea1c20e13d225cd9718f98ca216f12
Jeremy Barnes <jeremy@datacratic.com>
2016-10-01 17:59:34 +0000

    Added tinyxpath XPath processor (MLDB-1162)

--- html/ext/html_ext.mk
+++ html/ext/html_ext.mk
@@ -1,2 +1,3 @@
 $(eval $(call include_sub_make,libparserutils,libparserutils,../parserutils.mk))
 $(eval $(call include_sub_make,libhubbub,libhubbub,../hubbub.mk))
+$(eval $(call include_sub_make,tinyxpath,tinyxpath,../tinyxpath.mk))
