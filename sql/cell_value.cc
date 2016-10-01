88ab5ead180d23047ad4bf5c6cebaea153e56192
Jeremy Barnes <jeremy@datacratic.com>
2016-10-01 17:56:05 +0000

    Include escapable characters, whitespace in strings for JSON CellValue representation (MLDB-XXXX)

--- sql/cell_value.cc
+++ sql/cell_value.cc
@@ -1381,9 +1381,15 @@ extractStructuredJson(JsonPrintingContext & context) const
         const unsigned char * p = blobData();
         const unsigned char * e = p + blobLength();
 
+        auto isString = [] (char c)
+            {
+                return (c >= ' ' && c < 127)
+                    || (c == '\n' || c == '\r' || c == '\t');
+            };
+
         while (p < e) {
             const unsigned char * s = p;
-            while (p < e && *p >= ' ' && *p < 127 && isascii(*p))
+            while (isString(*p))
                 ++p;
             size_t len = p - s;
             //cerr << "len = " << len << endl;
@@ -1395,8 +1401,8 @@ extractStructuredJson(JsonPrintingContext & context) const
                 context.newArrayElement();
                 context.writeString((const char *)s, len);
             }
-                
-            while (p < e && (*p <= ' ' || *p >= 127 || !isascii(*p))) {
+
+            while (p < e && !isString(*p)) {
                 context.newArrayElement();
                 context.writeInt(*p++);
             }
