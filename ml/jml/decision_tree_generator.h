f5feaf930efde7f30f02ccd2f022caf5f803d7b6
François-Michel L'Heureux <fmlheureux@datacratic.com>
2016-10-05 21:01:51 +0000

    clang fixes

--- ml/jml/decision_tree_generator.h
+++ ml/jml/decision_tree_generator.h
@@ -38,15 +38,15 @@ public:
               std::vector<std::string> & unparsedKeys) override;
     
     /** Return to the default configuration. */
-    virtual void defaults();
+    virtual void defaults() override;
 
     /** Return possible configuration options. */
-    virtual Config_Options options() const;
+    virtual Config_Options options() const override;
 
     /** Initialize the generator, given the feature space to be used for
         generation. */
     virtual void init(std::shared_ptr<const Feature_Space> fs,
-                      Feature predicted);
+                      Feature predicted) override;
 
     using Classifier_Generator::generate;
 
@@ -57,7 +57,7 @@ public:
              const Training_Data & validation_data,
              const distribution<float> & training_weights,
              const distribution<float> & validation_weights,
-             const std::vector<Feature> & features, int recursion) const;
+             const std::vector<Feature> & features, int recursion) const override;
 
     /** Generate a classifier for boosting. */
     virtual std::shared_ptr<Classifier_Impl>
@@ -66,7 +66,7 @@ public:
              const boost::multi_array<float, 2> & weights,
              const std::vector<Feature> & features,
              float & Z,
-             int recursion) const;
+             int recursion) const override;
 
     int max_depth;
     int trace;
