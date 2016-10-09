71d2d693e9a9623b97c44f9be4c348dc0cf96c30
Jeremy Barnes <jeremy@datacratic.com>
2016-10-09 14:47:19 +0000

    Build working on aarch64, arm and x86_64 (MLDB-1940)

--- ext/pffft.mk
+++ ext/pffft.mk
@@ -1,6 +1,6 @@
 PFFFT_SOURCE = pffft.c fftpack.c
 
-HAS_FFTW3:=$(wildcard /usr/include/fftw3.*)
+HAS_FFTW3:=$(if $(call seq,$(ARCH),$(HOSTARCH)),$(wildcard /usr/include/fftw3.*))
 $(eval $(call set_compile_option,test_pffft.c,$(if $(HAS_FFTW3),-DHAVE_FFTW)))
 
 $(eval $(call library,pffft,$(PFFFT_SOURCE)))
