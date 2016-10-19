4b4cb2c863073034d0f02e8a445cd33feea8cb10
François-Michel L'Heureux <fmlheureux@datacratic.com>
2016-10-19 12:49:30 +0000

    Fix post_run_and_track_procedure_test.py

--- testing/post_run_and_track_procedure_test.py
+++ testing/post_run_and_track_procedure_test.py
@@ -8,30 +8,34 @@ mldb = mldb_wrapper.wrap(mldb)  # noqa
 
 class FakeLogger(object):
 
-    def __init__(self):
+    def __init__(self, mldb):
         self.records = []
+        self.mldb = mldb
+
+    def __enter__(self):
+        self.mldb_log = self.mldb.log
+        self.mldb.log = self.log
+        return self
+
+    def __exit__(self, ex_type, ex_val, tb):
+        self.mldb.log = self.mldb_log
 
     def log(self, msg):
+        self.mldb_log(msg)
         self.records.append(str(msg))
 
 class PostRunAndTrackProcedureTest(MldbUnitTest):  # noqa
 
     def test_it(self):
-        fl = FakeLogger()
-
-        # This test works well alone. mldb.log still points to fl.log after it
-        # so if tests need to be added an RAII structure to restore mldb.log
-        # would be needed.
-        mldb.log = fl.log
-
-        mldb.post_run_and_track_procedure({
-            'type' : 'mock',
-            'params' : {
-                'durationMs' : 4000,
-                'refreshRateMs' : 500
-            }
-        }, 1)
-        self.assertGreaterThan(len(fl.records), 0)
+        with FakeLogger(mldb) as fl:
+            mldb.post_run_and_track_procedure({
+                'type' : 'mock',
+                'params' : {
+                    'durationMs' : 4000,
+                    'refreshRateMs' : 500
+                }
+            }, 1)
+            self.assertGreater(len(fl.records), 0)
 
 if __name__ == '__main__':
     mldb.run_tests()
