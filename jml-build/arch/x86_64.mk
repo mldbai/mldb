c3d25cf65d5578765e35e8ec4432ca3cbaedd539
Jean Raby <jean@raby.sh>
2016-11-25 19:48:43 +0000

    Add cuda include and lib dirs on x86_64

--- jml-build/arch/x86_64.mk
+++ jml-build/arch/x86_64.mk
@@ -1,3 +1,4 @@
-ARCHFLAGS:=-fPIC -m64 -fno-omit-frame-pointer -msse3 -msse4.2
+ARCHFLAGS:=-fPIC -m64 -fno-omit-frame-pointer -msse3 -msse4.2 -L/usr/local/cuda-8.0/targets/x86_64-linux/lib
+CUDA_ARCH_INCLUDE_DIR:=/usr/local/cuda-8.0/targets/x86_64-linux/include/
 
 
