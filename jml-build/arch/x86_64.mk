58bde09a9e2c5b2913466a4c1a9ac71cac24bdc0
Jean Raby <jean@raby.sh>
2016-11-28 16:01:13 +0000

    Fixup CUDA_LIBRARY_PATH

--- jml-build/arch/x86_64.mk
+++ jml-build/arch/x86_64.mk
@@ -1,4 +1,4 @@
-ARCHFLAGS:=-fPIC -m64 -fno-omit-frame-pointer -msse3 -msse4.2 -L/usr/local/cuda-8.0/targets/x86_64-linux/lib
+ARCHFLAGS:=-fPIC -m64 -fno-omit-frame-pointer -msse3 -msse4.2
 CUDA_ARCH_INCLUDE_DIR:=/usr/local/cuda-8.0/targets/x86_64-linux/include/
 
 
