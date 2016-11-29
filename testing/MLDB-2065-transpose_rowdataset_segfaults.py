0b5102b0a6fa113330da2865145c33b32040ec3f
Francois Maillet <francois@recoset.com>
2016-11-29 19:47:11 +0000

    [MLDB-2065] Added failing test case

---
+++ testing/MLDB-2065-transpose_rowdataset_segfaults.py
@@ -0,0 +1,30 @@
+#
+# MLDB-2065-transpose_rowdataset_segfaults.py
+# Francois Maillet, 2016-11-29
+# This file is part of MLDB. Copyright 2016 Datacratic. All rights reserved.
+#
+
+mldb = mldb_wrapper.wrap(mldb)  # noqa
+
+class MLDB2065TransposeRowdatasetSegfaults(MldbUnitTest):  # noqa
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    def test_it(self):
+        self.assertTableResultEquals(
+            mldb.query("SELECT * FROM row_dataset([0, 0])"),
+            [
+                ["_rowName","column","value"],
+                ["0","0",0],
+                ["1","1",0]
+            ])
+
+        # this segfault
+        mldb.query("""
+            SELECT * FROM transpose((row_dataset([0, 0])))
+        """)
+
+if __name__ == '__main__':
+    mldb.run_tests()
