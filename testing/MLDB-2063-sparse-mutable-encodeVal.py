9a6be49d93794f80c01e16b06a4beeb21f8c0c99
Francois Maillet <francois@recoset.com>
2016-11-29 17:25:55 +0000

    [MLDB-2063] Added failing test case

---
+++ testing/MLDB-2063-sparse-mutable-encodeVal.py
@@ -0,0 +1,42 @@
+#
+# MLDB-2063-sparse-mutable-encodeVal.py
+# Francois Maillet, 2016-11-29
+# This file is part of MLDB. Copyright 2016 Datacratic. All rights reserved.
+#
+
+mldb = mldb_wrapper.wrap(mldb)  # noqa
+
+class MLDB2063SparseMutableEncodeVal(MldbUnitTest):  # noqa
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    def test_it(self):
+        print mldb.post('/v1/procedures', {
+                'type': 'transform',
+                'params': {
+                    'inputData': """
+                    SELECT
+                        column AS title
+                    FROM (SELECT * FROM 
+                        row_dataset({
+                            "My Value": 1
+                    }))
+                    """,
+                    'outputDataset': "keywords"
+                }
+            })
+
+
+        self.assertTableResultEquals(
+            mldb.query("select x from keywords"),
+            [
+                ["_rowName", "title"],
+                [       "0",  "My Value" ]
+            ]
+        )
+ 
+
+if __name__ == '__main__':
+    mldb.run_tests()
--- testing/testing.mk
+++ testing/testing.mk
@@ -450,3 +450,4 @@ $(eval $(call mldb_unit_test,post_run_and_track_procedure_test.py))
 $(eval $(call mldb_unit_test,MLDB-2022-multiple-prediction-example.js))
 
 $(eval $(call mldb_unit_test,MLDB-2043_tabular_big_int.py))
+$(eval $(call mldb_unit_test,MLDB-2063-sparse-mutable-encodeVal.py))
