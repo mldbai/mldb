0fac80ea6f5ccb0400bc9d1cd9e52a22f5944068
François-Michel L'Heureux <fmlheureux@datacratic.com>
2016-12-20 21:44:46 +0000

    rm comments

--- testing/MLDB-2100_fetcher_timeout_test.py
+++ testing/MLDB-2100_fetcher_timeout_test.py
@@ -21,11 +21,8 @@ socket.setdefaulttimeout(10)
 
 mldb = mldb_wrapper.wrap(mldb)  # noqa
 
-# create an INET, STREAMing socket
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# bind the socket to a public host, and a well-known port
 serversocket.bind(('127.0.0.1', 0))
-# become a server socket
 serversocket.listen(1)
 
 port_num = serversocket.getsockname()[1]
