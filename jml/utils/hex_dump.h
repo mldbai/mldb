941efdf665a026e02d6a7acf8b6863a7cdc273f9
Jeremy Barnes <jeremy@datacratic.com>
2017-05-12 01:30:48 +0000

    Header fixups (TRIVIAL)

--- jml/utils/hex_dump.h
+++ jml/utils/hex_dump.h
@@ -1,16 +1,13 @@
-// This file is part of MLDB. Copyright 2015 mldb.ai inc. All rights reserved.
-
 /* hex_dump.h                                                      -*- C++ -*-
    Jeremy Barnes, 6 October 2010
    Copyright (c) 2010 Jeremy Barnes.  All rights reserved.
    Copyright (c) 2010 mldb.ai inc.  All rights reserved.
+   This file is part of MLDB. Copyright 2015 mldb.ai inc. All rights reserved.
 
    Routine to dump memory in hex format.
 */
 
-#ifndef __utils__hex_dump_h__
-#define __utils__hex_dump_h__
-
+#pragma once
 #include <stddef.h>
 
 namespace ML {
@@ -22,5 +19,3 @@ void hex_dump(const void * mem, size_t total_memory, size_t max_size = 1024);
 
 
 } // namespace ML
-
-#endif /* __utils__hex_dump_h__ */
