9113e6c08d7da8000808aee6cc4d841547ac3ca8
Jeremy Barnes <jeremy@datacratic.com>
2018-06-13 14:20:45 +0000

    Start looking at inifiniband (WIP)

--- vfs_handlers/testing/testing.mk
+++ vfs_handlers/testing/testing.mk
@@ -2,3 +2,4 @@
 $(eval $(call test,fs_utils_test,vfs_handlers test_utils arch value_description types vfs,boost manual))
 #$(eval $(call test,azure_blob_storage_test,vfs vfs_handlers $(STD_FILESYSTEM_LIBNAME) boost_system,boost))
 $(eval $(call test,memory_map_test,vfs_handlers,boost))
+$(eval $(call test,infiniband_test,ibverbs,boost))
