18d985023ce346b41a0aea48683a1df0e6b5d7ea
Jeremy Barnes <jeremy@barneso.com>
2018-08-06 11:30:53 -0400

    Plugins cleanup (TRIVIAL)

--- plugins/ui/ui.mk
+++
@@ -1,6 +0,0 @@
-# This file is part of MLDB. Copyright 2015 mldb.ai inc. All rights reserved.
-
-# Makefile for ui plugins
-# Jeremy Barnes, 4 March 2015
-
-$(eval $(call library,mldb_ui_plugins,,value_description))
