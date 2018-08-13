2aa420c9f72286f177192d98dbb9f14ac49714db
Jeremy Barnes <jeremy@barneso.com>
2018-08-13 14:18:54 -0400

    Use embed_file rather than python-as-string-literal-in-c++ for python wrapper (python3)

--- builtin/python/mldb_python_plugin.mk
+++ builtin/python/mldb_python_plugin.mk
@@ -1,12 +1,11 @@
 # This file is part of MLDB. Copyright 2015 mldb.ai inc. All rights reserved.
 
-
-
 PYTHON_PLUGIN_SOURCES := \
 	python_loader.cc \
 	python_plugin_context.cc \
 	python_entities.cc \
-	python_converters.cc
+	python_converters.cc \
+	mldb_wrapper.py.embed
 
 # Needed so that Python plugin can find its header
 $(eval $(call set_compile_option,$(PYTHON_PLUGIN_SOURCES),-I$(PYTHON_INCLUDE_PATH)))
