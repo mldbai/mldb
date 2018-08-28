4ecefb70270f4fe560480b2cfdd4671c1de2ae19
Jeremy Barnes <jeremy@barneso.com>
2018-08-28 08:07:07 -0400

    Added more Python src_utils (TRIVIAL)

---
+++ src_tools/ident_replace_all_python.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ $# -lt 2 ] ;
+    then
+    echo "usage: " $0 "<regex> <replacement> [<directory>]";
+    echo "(directory defaults to '.')";
+    exit;
+fi
+
+REGEX=$1
+REPLACEMENT=$2
+DIR="."
+if [ $# -eq 3 ];
+    then
+    DIR=$3
+fi
+
+FILES=`find $DIR -name "*.py" | xargs grep -l "$REGEX"`
+
+#echo "files = " $FILES
+
+for file in $FILES ;
+  do
+  echo $file
+  cp -f $file $file~
+  cat $file~ | sed "s!$REGEX!$REPLACEMENT!g" > $file~~ && mv $file~~ $file
+  chmod --reference=$file~ $file
+done
