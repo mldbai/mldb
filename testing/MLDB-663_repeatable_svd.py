4b113331e1bbac0e2750081b54114eebe4372a23
Jeremy Barnes <jeremy@barneso.com>
2018-08-31 11:34:23 -0400

    MLDB-663 test no longer manual (TRIVIAL)

--- testing/MLDB-663_repeatable_svd.py
+++ testing/MLDB-663_repeatable_svd.py
@@ -1,6 +1,7 @@
 # This file is part of MLDB. Copyright 2015 mldb.ai inc. All rights reserved.
 
 import json
+from mldb import mldb
 
 result = mldb.perform("PUT", "/v1/datasets/rcp", [], {
     'type' : 'beh',
