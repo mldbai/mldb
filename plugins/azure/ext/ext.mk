13b5ab484fc3cff8c67987cb54e7fd2752356623
Jeremy Barnes <jeremy@barneso.com>
2018-08-31 11:31:11 -0400

    ext/xxx -> plugins/yyy/ext/; vfs/xxx.{h,cc} -> plugins/yyy/

---
+++ plugins/azure/ext/ext.mk
@@ -0,0 +1,4 @@
+# External programs and libraries used by Azure plugin
+
+#$(eval $(call include_sub_make,casablanca,casablanca,../casablanca.mk))
+#$(eval $(call include_sub_make,azure-storage-cpp,azure-storage-cpp,../azure-storage-cpp.mk))
