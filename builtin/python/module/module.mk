f8730036df22f8b0816c757ffcb0f7d5bfbce94e
Jeremy Barnes <jeremy@barneso.com>
2018-08-31 21:46:13 -0400

    FIXUP from mldb import mldb (python3)

---
+++ builtin/python/module/module.mk
@@ -0,0 +1,18 @@
+# This file is part of MLDB. Copyright 2015 mldb.ai inc. All rights reserved.
+
+
+MLDB_PYTHON_ADDON_SOURCES := \
+	find_mldb_environment.cc
+
+MLDB_PYTHON_ADDON_LINK := \
+	boost_python3 mldb_python_plugin
+
+$(eval $(call python_addon,_mldb,$(MLDB_PYTHON_ADDON_SOURCES),$(MLDB_PYTHON_ADDON_LINK)))
+
+$(eval $(call set_compile_option,$(PYTHON_PLUGIN_SOURCES),-I$(PYTHON_INCLUDE_PATH)))
+
+$(eval $(call python_module,mldb,$(notdir $(wildcard $(CWD)/*.py)),_mldb))
+
+
+# Our mldb_runner binary requires the plugin to be present
+$(BIN)/mldb_runner:	| $(PYTHON_mldb_DEPS)
