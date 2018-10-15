f5340ef3d34f0b5da9aa25be3a7ff014b0346af5
Jeremy Barnes <jeremy@elementai.com>
2018-10-15 01:04:29 +0000

    Memory region forward definitions (TRIVIAL)

---
+++ block/memory_region_fwd.h
@@ -0,0 +1,26 @@
+/** memory_region.h                                                -*- C++ -*-
+    Jeremy Barnes, 27 March 2016
+    This file is part of MLDB. Copyright 2016 mldb.ai inc. All rights reserved.
+
+    Basic primitives around memory regions.  Once frozen, these are the
+    representation that covers CPU memory, device memory and remote
+    memory and implements the primitives that allow data to be made
+    available and brought to the compute resources required.
+*/
+
+#pragma once
+
+namespace MLDB {
+
+struct FrozenMemoryRegion;
+template<typename T>
+struct FrozenMemoryRegionT;
+struct MutableMemoryRegion;
+template<typename T>
+struct MutableMemoryRegionT;
+
+struct MappedSerializer;
+struct StructuredSerializer;
+struct StructuredReconstituter;
+
+} // namespace MLDB
