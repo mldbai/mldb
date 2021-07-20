776082574223ec1e778bb5c16a40ec47be427b44
Jeremy Barnes <jeremy@barneso.com>
2021-07-20 07:06:47 -0400

    Vendored cryptopp (OSX)

--- vfs_handlers/aws/aws.mk
+++ vfs_handlers/aws/aws.mk
@@ -7,7 +7,7 @@ LIBAWS_SOURCES := \
 	aws.cc \
 	sqs.cc \
 
-LIBAWS_LINK := credentials hash crypto++ tinyxml2 http rest
+LIBAWS_LINK := credentials hash cryptopp tinyxml2 http rest
 
 $(eval $(call library,aws,$(LIBAWS_SOURCES),$(LIBAWS_LINK)))
 
@@ -15,7 +15,7 @@ $(eval $(call library,aws_vfs_handlers,s3_handlers.cc,aws vfs tinyxml2 http))
 
 
 # gcc 4.7
-$(eval $(call set_compile_option,aws.cc,-fpermissive))
+$(eval $(call set_compile_option,aws.cc,-fpermissive -I $(CRYPTOPP_INCLUDE_DIR)))
 
 $(eval $(call program,s3tee,aws_vfs_handlers boost_program_options utils))
 $(eval $(call program,s3cp,aws_vfs_handlers boost_program_options utils))
