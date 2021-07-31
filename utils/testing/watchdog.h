f9a07e3c8156db327891ccf2d7893fa8ae4c972a
Jeremy Barnes <jeremy@barneso.com>
2021-07-31 12:02:36 -0400

    FIXUP watchdog: don't kill process group, just this process

--- utils/testing/watchdog.h
+++ utils/testing/watchdog.h
@@ -11,6 +11,7 @@
 #include <thread>
 #include <iostream>
 #include <signal.h>
+#include <unistd.h>
 
 namespace MLDB {
 
@@ -26,7 +27,7 @@ struct Watchdog {
 
         cerr << "**** WATCHDOG TIMEOUT; KILLING HUNG TEST ****"
              << endl;
-        kill(0, SIGKILL);
+        kill(getpid(), SIGKILL);
         abort();
     }
     
