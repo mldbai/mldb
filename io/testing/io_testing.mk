b3ab73be910937b6c8e620633bcfb7b3b5a5235f
Jeremy Barnes <jeremy@barneso.com>
2021-08-07 16:55:40 -0400

    Run asan tests as part of CI/CD (asan)

--- io/testing/io_testing.mk
+++ io/testing/io_testing.mk
@@ -1,7 +1,7 @@
 # This file is part of MLDB. Copyright 2015 mldb.ai inc. All rights reserved.
 
 $(eval $(call test,asio_timer_test,io_base watch types,boost))
-$(eval $(call test,async_writer_bench,io_base arch types,boost))
+$(eval $(call test,async_writer_bench,io_base arch types,boost manual))
 $(eval $(call test,epoll_test,io_base,boost))
 $(eval $(call test,epoller_test,io_base,boost))
 $(eval $(call test,timerfd_test,io_base,boost))
