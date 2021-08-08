ae30710250dc70fe6a010e6dd7610646e18078df
Jeremy Barnes <jeremy@barneso.com>
2021-08-08 17:19:49 -0400

    Logger: check buffer bounds for printf type functions (asan)

--- logging/logging.cc
+++ logging/logging.cc
@@ -333,10 +333,16 @@ std::ostream & Logging::Category::beginWrite(char const * fct, char const * file
 
     timeval now;
     gettimeofday(&now, 0);
-    char text[64];
+    constexpr size_t BUF_SIZE = 64;
+    char text[BUF_SIZE];
     auto count = strftime(text, sizeof(text), "%Y-%m-%d %H:%M:%S", localtime(&now.tv_sec));
+    if (count == 0)
+        throw Exception("strftime in log: buffer too short");
     int ms = now.tv_usec / 1000;
-    sprintf(text + count, ".%03d", ms);
+    auto res = snprintf(text + count, sizeof(text) - count, ".%03d", ms);
+    if (res >= sizeof(text) - count)
+        throw Exception("strftime in log: buffer too short");
+
     data->writer->head(text, data->name, fct, file, line);
     return data->stream;
 }
