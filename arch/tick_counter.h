6f14e93591a499d9886096af0e06aad95dd7be69
Jeremy Barnes <jeremy@barneso.com>
2021-08-09 20:16:54 +0000

    Disabled tick counter tests on arm64 as tsc is not always userspace accessible (arm64)

--- arch/tick_counter.h
+++ arch/tick_counter.h
@@ -33,6 +33,11 @@ MLDB_ALWAYS_INLINE uint64_t ticks()
 
     return result;
 # endif // 32/64 bits
+#elif (defined(MLDB_ARM_ISA) && 0) // not accessible from user space 
+    uint64_t result;
+    // read the current counter
+    asm volatile ("mrs %0, cntpct_el0" : "=r"(result));
+    return result;
 #else // non-intel
     return 0;  
 #endif
