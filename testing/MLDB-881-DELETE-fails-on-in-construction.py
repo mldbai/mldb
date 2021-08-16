2abf68cf662d85d7a8bbeb36a95d2b4d047d49e4
Jeremy Barnes <jeremy@barneso.com>
2021-08-16 06:57:42 -0400

    Fixed race condition between initialization/status in MLDB-881 test (TRIVIAL)

--- testing/MLDB-881-DELETE-fails-on-in-construction.py
+++ testing/MLDB-881-DELETE-fails-on-in-construction.py
@@ -6,24 +6,31 @@
 
 from mldb import mldb, ResponseException
 
+# Do non-trivial work as we don't want trival creation to race with returning
+# the first status call
+
+config = {
+    "type": "python",
+    "params":{
+        "source":{
+            "routes": "from mldb import mldb\nmldb.log(str(request.rest_params))\nmldb.log(str(request.payload))\nrequest.set_return({'args': request.rest_params, 'payload': request.payload})"
+        }
+    }
+}
 
 # create an expensive resource async
-resp = mldb.put_async("/v1/datasets/dummy2", {
-    'type' : 'import.text',
-    'params' : {
-        'dataFileUrl': 'file://mldb/mldb_test_data/reddit.csv.zst'
-    }
-})
+resp = mldb.put_async("/v1/plugins/dummy2", config)
+
 assert resp.json()['state'] == 'initializing', \
     'the resource should still be under construction'
 
 # deleting that resource will wait until it is constructed
-resp = mldb.delete_async("/v1/datasets/dummy2")
+resp = mldb.delete_async("/v1/plugins/dummy2")
 assert resp.status_code == 204
 
 # once the DELETE returns the resource should have been deleted
 try:
-    mldb.get("/v1/datasets/dummy2")
+    mldb.get("/v1/plugins/dummy2")
 except ResponseException:
     pass
 else:
