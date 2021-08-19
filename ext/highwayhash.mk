dfafe45101f6a7ae17a8086e9a88724f10508dd4
Jeremy Barnes <jeremy@barneso.com>
2021-08-19 17:59:41 -0400

    FIXUP highwayhash (ubsan)

--- ext/highwayhash.mk
+++ ext/highwayhash.mk
@@ -4,7 +4,7 @@ HIGHWAYHASH_SOURCE_arm64 := hh_neon.cc
 HIGHWAYHASH_SOURCE_aarch64 := $(HIGHWAYHASH_SOURCE_arm64)
 
 HIGHWAYHASH_SOURCE = ../highwayhash.cc $(addprefix highwayhash/,sip_hash.cc hh_portable.cc arch_specific.cc instruction_sets.cc nanobenchmark.cc os_specific.cc c_bindings.cc $(HIGHWAYHASH_SOURCE_$(ARCH)))
-$(eval $(call set_compile_option,$(HIGHWAYHASH_SOURCE),-I$(CWD) -Wno-unused-private-field))
+$(eval $(call set_compile_option,$(HIGHWAYHASH_SOURCE),-I$(CWD) -Wno-unused-private-field -fsanitize=))
 
 ifeq ($(ARCH),x86_64)
 $(eval $(call set_compile_option,highwayhash/hh_sse41.cc,-msse4.1 -I$(CWD)))
