5d5af582945a783156012e342f470c153df366f0
Jeremy Barnes <jeremy@barneso.com>
2021-08-25 14:39:34 -0400

    Attempted fix for Python stream capture init (OSX)

--- builtin/python/capture_stream.cc
+++ builtin/python/capture_stream.cc
@@ -60,63 +60,68 @@ PyObject* Stdstream_flush(PyObject* self, PyObject* args)
     return Py_BuildValue("");
 }
 
-PyMethodDef Stdstream_methods[] =
-{
-    {"write", Stdstream_write, METH_VARARGS, "sys.stdstream.write"},
-    {"flush", Stdstream_flush, METH_VARARGS, "sys.stdstream.flush"},
-    {0, 0, 0, 0} // sentinel
-};
-
-PyTypeObject StdstreamType =
-{
-    PyVarObject_HEAD_INIT(0, 0)
-    "streamcapture.StdstreamType",     /* tp_name */
-    sizeof(Stdstream),       /* tp_basicsize */
-    0,                    /* tp_itemsize */
-    0,                    /* tp_dealloc */
-    0,                    /* tp_print */
-    0,                    /* tp_getattr */
-    0,                    /* tp_setattr */
-    0,                    /* tp_reserved */
-    0,                    /* tp_repr */
-    0,                    /* tp_as_number */
-    0,                    /* tp_as_sequence */
-    0,                    /* tp_as_mapping */
-    0,                    /* tp_hash  */
-    0,                    /* tp_call */
-    0,                    /* tp_str */
-    0,                    /* tp_getattro */
-    0,                    /* tp_setattro */
-    0,                    /* tp_as_buffer */
-    Py_TPFLAGS_DEFAULT,   /* tp_flags */
-    "streamcapture.Stdstream objects", /* tp_doc */
-    0,                    /* tp_traverse */
-    0,                    /* tp_clear */
-    0,                    /* tp_richcompare */
-    0,                    /* tp_weaklistoffset */
-    0,                    /* tp_iter */
-    0,                    /* tp_iternext */
-    Stdstream_methods,       /* tp_methods */
-    0,                    /* tp_members */
-    0,                    /* tp_getset */
-    0,                    /* tp_base */
-    0,                    /* tp_dict */
-    0,                    /* tp_descr_get */
-    0,                    /* tp_descr_set */
-    0,                    /* tp_dictoffset */
-    0,                    /* tp_init */
-    0,                    /* tp_alloc */
-    0,                    /* tp_new */
-};
-
-PyModuleDef streamcapturemodule =
-{
-    PyModuleDef_HEAD_INIT,
-    "streamcapture", 0, -1, 0,
-};
+// This will be reliably initialized when the module is loaded as a shared library,
+// unlike the PyTypeObject which is sometimes initialized AFTER we attempt to register
+// it with the Python runtime.
+static PyTypeObject * initializedStdstreamType = nullptr;
 
 PyMODINIT_FUNC PyInit_streamcapture(void)
 {
+    static PyMethodDef Stdstream_methods[] =
+    {
+        {"write", Stdstream_write, METH_VARARGS, "sys.stdstream.write"},
+        {"flush", Stdstream_flush, METH_VARARGS, "sys.stdstream.flush"},
+        {0, 0, 0, 0} // sentinel
+    };
+
+    static PyTypeObject StdstreamType =
+    {
+        PyVarObject_HEAD_INIT(0, 0)
+        "streamcapture.StdstreamType",     /* tp_name */
+        sizeof(Stdstream),       /* tp_basicsize */
+        0,                    /* tp_itemsize */
+        0,                    /* tp_dealloc */
+        0,                    /* tp_print */
+        0,                    /* tp_getattr */
+        0,                    /* tp_setattr */
+        0,                    /* tp_reserved */
+        0,                    /* tp_repr */
+        0,                    /* tp_as_number */
+        0,                    /* tp_as_sequence */
+        0,                    /* tp_as_mapping */
+        0,                    /* tp_hash  */
+        0,                    /* tp_call */
+        0,                    /* tp_str */
+        0,                    /* tp_getattro */
+        0,                    /* tp_setattro */
+        0,                    /* tp_as_buffer */
+        Py_TPFLAGS_DEFAULT,   /* tp_flags */
+        "streamcapture.Stdstream objects", /* tp_doc */
+        0,                    /* tp_traverse */
+        0,                    /* tp_clear */
+        0,                    /* tp_richcompare */
+        0,                    /* tp_weaklistoffset */
+        0,                    /* tp_iter */
+        0,                    /* tp_iternext */
+        Stdstream_methods,    /* tp_methods */
+        0,                    /* tp_members */
+        0,                    /* tp_getset */
+        0,                    /* tp_base */
+        0,                    /* tp_dict */
+        0,                    /* tp_descr_get */
+        0,                    /* tp_descr_set */
+        0,                    /* tp_dictoffset */
+        0,                    /* tp_init */
+        0,                    /* tp_alloc */
+        0,                    /* tp_new */
+    };
+
+    static PyModuleDef streamcapturemodule =
+    {
+        PyModuleDef_HEAD_INIT,
+        "streamcapture", 0, -1, 0,
+    };
+
     StdstreamType.tp_new = PyType_GenericNew;
     if (PyType_Ready(&StdstreamType) < 0) {
         cerr << "type not ready" << endl;
@@ -129,6 +134,9 @@ PyMODINIT_FUNC PyInit_streamcapture(void)
         Py_INCREF(&StdstreamType);
         PyModule_AddObject(m, "Stdstream", reinterpret_cast<PyObject*>(&StdstreamType));
     }
+
+    initializedStdstreamType = &StdstreamType;
+    
     return m;
 }
 
@@ -144,7 +152,8 @@ setStdStream(const EnterThreadToken & token,
              const std::string & streamName)
 {
     PyObject * oldStream = PySys_GetObject(streamName.c_str()); // borrowed
-    PyObject * newStream = StdstreamType.tp_new(&StdstreamType, 0, 0);
+    assert(initializedStdstreamType);
+    PyObject * newStream = initializedStdstreamType->tp_new(initializedStdstreamType, 0, 0);
     Stdstream* impl = reinterpret_cast<Stdstream*>(newStream);
     impl->write = std::move(write);
     
