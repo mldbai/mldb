db865888bad360d2682a6e640158ecc36b16b378
Jeremy Barnes <jeremy@barneso.com>
2021-09-02 15:06:44 -0400

    Added benchmark scripts for airlines

---
+++ testing/benchmark-10M-airlines.js
@@ -0,0 +1,87 @@
+var mldb = require('mldb')
+var unittest = require('mldb/unittest');
+
+var import_data_conf = {
+    type: "import.text",
+    params: {
+        dataFileUrl : "file://train-10m.csv.zst",
+        outputDataset: {
+            id: "airline",
+        },
+        runOnCreation: true,
+        encoding: 'ascii',
+        ignoreBadLines: true,
+    }
+};
+
+var start = new Date();
+var res = mldb.put("/v1/procedures/import_data", import_data_conf);
+var end = new Date();
+
+mldb.log(res.json);
+unittest.assertEqual(res.responseCode, 201);
+
+mldb.log("importing training data took", (end - start), "milliseconds");
+
+var import_test_conf = {
+    type: "import.text",
+    params: {
+        dataFileUrl : "file://test.csv.zst",
+        outputDataset: {
+            id: "airline_test",
+        },
+        runOnCreation: true,
+        encoding: 'ascii',
+        ignoreBadLines: true,
+    }
+};
+
+start = new Date();
+var res = mldb.put("/v1/procedures/import_test", import_test_conf)
+end = new Date();
+mldb.log(res.json);
+unittest.assertEqual(res.responseCode, 201);
+
+mldb.log("importing testing data took", (end - start), "milliseconds");
+
+var benchmark_conf = {
+    type: "randomforest.binary.train",
+    params: {
+        trainingData: "select {* EXCLUDING(dep_delayed_15min)} as features, dep_delayed_15min = 'Y' as label from airline",
+        runOnCreation: true,
+        modelFileUrl: "file://tmp/benchmark-airlines.cls",
+        functionName: "classifyme",
+        featureVectorSamplings : 5,
+        featureSamplings : 20,
+        maxDepth : 20,
+        verbosity : 0,
+    }
+};
+
+start = new Date();
+res = mldb.put("/v1/procedures/benchmark", benchmark_conf);
+end = new Date();
+mldb.log(res.json);
+unittest.assertEqual(res.responseCode, 201);
+
+mldb.log("training classifier took", (end - start), "milliseconds");
+
+var test_conf = {
+    type: "classifier.test",
+    params: {
+        testingData: "select classifyme({{* EXCLUDING(dep_delayed_15min)} as features})[score] as score, dep_delayed_15min = 'Y' as label from airline_test",
+        "runOnCreation": true,
+    }
+};
+
+start = new Date();
+res = mldb.put("/v1/procedures/test", test_conf);
+end = new Date();
+mldb.log(res.json);
+unittest.assertEqual(res.responseCode, 201);
+
+mldb.log("testing classifier took", (end - start), "milliseconds");
+
+mldb.log("auc =", res.json.status.firstRun.status.auc)
+
+"success"
