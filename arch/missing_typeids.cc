6de35efc452e3ab3d4ba338f5b32bc1f688d50f0
Jeremy Barnes <jeremy@barneso.com>
2025-01-16 19:40:49 -0500

    FIXUP missing type_info for half (un-break Linux)

--- arch/missing_typeids.cc
+++ arch/missing_typeids.cc
@@ -1,6 +1,8 @@
 #include <typeinfo>
 #include <iostream>
 
+#ifdef __clang__
+#ifdef __APPLE__
 // Sometimes the typeinfo node for _Float16 is missing (llvm on OSX, at least)
 // Create it here if that's the case
 struct HalfTypeInfo: public std::type_info {
@@ -8,4 +10,5 @@ struct HalfTypeInfo: public std::type_info {
 };
 
 HalfTypeInfo _ZTIDF16_ __attribute__((__weak__));
-
+#endif /* __APPLE__ */
+#endif /* __clang__ */
